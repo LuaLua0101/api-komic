b152e7ec36fba5f5899c691f6a24102bec15d7ad
NAD <lualua0909@gmail.com>
2020-02-28 15:46:19 +0700

    a

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-
 Route::get('test-api', 'Api\ApiController@testApi');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
