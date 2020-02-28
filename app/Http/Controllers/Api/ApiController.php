e6a930bf453fad306bf8a70b89d15e421c8dfee9
NAD <lualua0909@gmail.com>
2020-02-28 16:21:56 +0700

    commit all

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 
 class ApiController extends Controller
 {
@@ -10,5 +11,11 @@ class ApiController extends Controller
     public function testApi()
     {
         return 'OK';
+    }
+
+    public function getComics()
+    {
+        $comics = Comic::get();
+        return response()->json([$comics], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::get('test-api', 'Api\ApiController@testApi');
+Route::get('comics', 'Api\ApiController@getComics');
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
