50c7f6930f576fbbaa25b437065d5cbccc63a17b
NAD <mtv.duyna@gmail.com>
2020-04-09 17:30:49 +0700

    xc

--- app/Http/Controllers/Api/JobController.php
+++ app/Http/Controllers/Api/JobController.php
@@ -7,6 +7,7 @@ use App\Models\Block;
 use App\Models\Company;
 use App\Models\Degree;
 use App\Models\Job;
+use App\Models\Province;
 use Illuminate\Http\Request;
 
 class JobController extends Controller
@@ -61,7 +62,7 @@ class JobController extends Controller
     public function getJobs()
     {
         $data = Job::getList(1);
-
+        $data['province'] = Province::getById($data->province_id);
         return response()->json(['data' => $data], 200);
     }
 }
